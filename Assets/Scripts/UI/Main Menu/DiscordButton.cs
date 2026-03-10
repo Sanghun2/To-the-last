4ef2b254a2d7e246242b71a440547a735cea5507
lshzang86 <lshzang86@naver.com>
2026-03-10 22:11:14 +0900

    [Asset Import] vFolder

---
+++ Assets/Scripts/UI/Main Menu/DiscordButton.cs
@@ -0,0 +1,11 @@
+﻿using BilliotGames;
+using UnityEngine;
+
+public class DiscordButton : ButtonBase
+{
+    [SerializeField] string discordURL = "https://discord.gg/aHkuXAwfn3";
+
+    protected override void ButtonAction() {
+        Application.OpenURL(discordURL);
+    }
+}
---
+++ Assets/Scripts/UI/Main Menu/ShopButton.cs
@@ -0,0 +1,9 @@
+﻿using BilliotGames;
+using UnityEngine;
+
+public class ShopButton : ButtonBase
+{
+    protected override void ButtonAction() {
+        throw new System.NotImplementedException();
+    }
+}
