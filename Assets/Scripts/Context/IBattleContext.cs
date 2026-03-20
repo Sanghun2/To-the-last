2f0f2fbe9de544a96db4812c1baa6dbe8178a73a
lshzang86 <lshzang86@naver.com>
2026-03-21 00:59:02 +0900

    Condition Check에 사용할 Context 인터페이스 추가

---
+++ Assets/Scripts/Context/IBattleContext.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+public interface IBattleContext
+{
+    BattleEntity Caster { get; }
+    List<BattleEntity> Targets { get; }
+}
---
+++ Assets/Scripts/Context/IInventoryContext.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using BilliotGames;
+using UnityEngine;
+
+public interface IInventoryContext
+{
+    IReadOnlyList<InventoryBase> Inventories { get; }
+}
