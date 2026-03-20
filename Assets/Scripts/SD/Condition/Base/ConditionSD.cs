4835b755188c835edc07c5c2b113151efbad8361
lshzang86 <lshzang86@naver.com>
2026-03-21 01:42:28 +0900

    ConditionSD 구조 추가

---
+++ Assets/Scripts/SD/Condition/Base/ConditionSD.cs
@@ -0,0 +1,9 @@
+﻿using BilliotGames;
+using UnityEngine;
+
+public interface IconditionContext { }
+
+public abstract class ConditionSD : SDBase
+{
+    public abstract bool IsMet(IconditionContext context);
+}
--- Assets/Scripts/SD/Condition/ConditionSD.cs
+++
@@ -1,7 +0,0 @@
-﻿using BilliotGames;
-using UnityEngine;
-
-public abstract class ConditionSD : SDBase
-{
-    public abstract bool IsMet(BattleEntity caster, BattleEntity target);
-}
