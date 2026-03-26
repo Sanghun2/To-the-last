fe453fbce076fb5eb1a30c283adbf683e34c9a82
lshzang86 <lshzang86@naver.com>
2026-03-26 19:00:42 +0900

    마박사 데이터 할당

--- Assets/Scripts/SD/Entity/Character/CharacterSD.cs
+++ Assets/Scripts/SD/Entity/Character/CharacterSD.cs
@@ -9,10 +9,12 @@ public class CharacterSD : EntitySDBase
     public string[] Features => features;
     public bool IsDefaultCharacter => isDefaultCharacter;
     public IReadOnlyList<MetabolismData> MetabolismDatas => metabolismSD.ConsumeInfos;
+    public IReadOnlyList<StatData> StatList => statList;
 
     [SerializeField] string[] features;
     [SerializeField] bool isDefaultCharacter;
     [SerializeField] MetabolismSD metabolismSD;
+    [SerializeField] List<StatData> statList;
 
     protected virtual void OnValidate() {
         RenameAsset(ID, suffix: $"_CharacterSD");
